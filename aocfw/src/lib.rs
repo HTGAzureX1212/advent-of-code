1435d4424aca45145b31a710717a3a5e417dfd0a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-28 13:29:02 +0800

    add solution trait

--- aocfw/src/lib.rs
+++ aocfw/src/lib.rs
@@ -1,1 +1,11 @@
-// new lib
+use std::fmt::Debug;
+
+pub trait Solution<'a> {
+    type ParsedT: Clone;
+    type Part1OutputT: Debug + PartialEq;
+    type Part2OutputT: Debug + PartialEq;
+
+    fn parse(input: &'a str) -> Self::ParsedT;
+    fn part_1(data: Self::ParsedT) -> Self::Part1OutputT;
+    fn part_2(data: Self::ParsedT) -> Self::Part2OutputT;
+}
