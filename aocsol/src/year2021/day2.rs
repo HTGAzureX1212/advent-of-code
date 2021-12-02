50f7618fd1f7c78cab15b754e8252a54614c8b4a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-02 09:42:31 +0800

    adfd year 2021 day 2 framework

---
+++ aocsol/src/year2021/day2.rs
@@ -0,0 +1,20 @@
+use aocfw::Solution;
+
+pub struct Year2021Day2;
+
+impl<'a> Solution<'a> for Year2021Day2 {
+    type ParsedT = ();
+    type Part1OutputT = ();
+    type Part2OutputT = ();
+
+    fn parse(input: &'a str) -> Self::ParsedT {
+    }
+
+    fn part_1(data: Self::ParsedT) -> Self::Part1OutputT {
+    }
+
+    fn part_2(data: Self::ParsedT) -> Self::Part2OutputT {
+    }
+}
+
+aocfw::solve_fn!(fn solve; struct Year2021Day2; filename "input/day2"; year 2021; day 2);
--- aocsol/src/year2021/mod.rs
+++ aocsol/src/year2021/mod.rs
@@ -1,4 +1,5 @@
 #[cfg_attr(feature = "year2021-day1", path = "day1.rs")]
+#[cfg_attr(feature = "year2021-day2", path = "day2.rs")]
 pub mod day;
 
 pub fn run() {
