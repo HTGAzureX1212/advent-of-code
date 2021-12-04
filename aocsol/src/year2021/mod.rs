de8460c7d3ab172889c1435882338a7dd2725489
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-12-04 11:44:58 +0800

    add year 2021 day 4

--- aocsol/src/year2021/mod.rs
+++ aocsol/src/year2021/mod.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(feature = "year2021-day1", path = "day1.rs")]
 #[cfg_attr(feature = "year2021-day2", path = "day2.rs")]
 #[cfg_attr(feature = "year2021-day3", path = "day3.rs")]
+#[cfg_attr(feature = "year2021-day4", path = "day4.rs")]
 pub mod day;
 
 pub fn run() {
